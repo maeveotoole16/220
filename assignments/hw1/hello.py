1568ef6551604f5aa0043a386006363f78c20fa0
maeveotoole16 <maeveotoole16@gmail.com>
2021-09-02 12:49:07 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Maeve O'Toole
+
+hello.py
+
+Problem: This is the first homework assignment that aims to run two
+tests, a static test as well as a linting test, on the code in order
+to print out the wanted statement, in this case, 'hello, world!'
+
+Certificate of Authenticity: This assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+main()
